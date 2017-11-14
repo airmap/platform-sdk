d8ac9083dd145c5fe67b5731ab8ac43eaeaf04d6
Thomas Voß <thomas.voss.bochum@gmail.com>
2017-11-14 10:26:15 +0100

    Force --yes --force-yes for apt operations

--- tools/ubuntu/build-package.sh
+++ tools/ubuntu/build-package.sh
@@ -4,9 +4,9 @@ set -e
 
 apt update
 # TODO(tvoss): We should host the protobuf3 packages in a PPA we own.
-apt install software-properties-common
+apt --yes --force-yes install software-properties-common
 add-apt-repository -y ppa:maarten-fonville/protobuf
 apt update
-apt build-dep -y ./
-apt upgrade -y
+apt --yes build-dep ./
+apt --yes --force-yes upgrade
 dpkg-buildpackage -j4
