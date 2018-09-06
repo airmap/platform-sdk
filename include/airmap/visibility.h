1808423e2d5c0e1c0bddc410e7341786bae15f23
Thomas Voß <thomas@airmap.com>
2018-09-06 15:30:18 +0200

    [platform-sdk] Set symbol visibility to hidden

---
+++ include/airmap/visibility.h
@@ -0,0 +1,22 @@
+//
+//  traffic.h
+//  AirMap Platform SDK
+//
+//  Copyright © 2018 AirMap, Inc. All rights reserved.
+//
+
+#ifndef AIRMAP_VISIBILITY_H_
+#define AIRMAP_VISIBILITY_H_
+
+#if defined(__GNUC__)
+#define AIRMAP_EXPORT __attribute__((visibility("default")))
+#elif defined(__clang__)
+#define AIRMAP_EXPORT __attribute__((visibility("default")))
+#elif defined(_MSC_VER)
+#define AIRMAP_EXPORT __declspec(dllexport)
+#else
+#pragma message("unknown compiler - default AIRMAP_EXPORT to empty")
+#define AIRMAP_EXPORT
+#endif
+
+#endif  // AIRMAP_VISIBILITY_H_
