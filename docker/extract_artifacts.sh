8e6b23a2c22a9a25bee14d59ce9d0a609368461e
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-02 10:43:00 -0600

    extract and store pipeline artifacts

---
+++ docker/extract_artifacts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE=${1:-platform-sdk.ubuntu}
+ARTIFACT_DIR="artifact_dir"
+
+mkdir -p ./$ARTIFACT_DIR
+
+DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find . -name airmap-platform-sdk*.deb")
+CONTAINER=$(docker create $IMAGE)
+for deb in $DEBS; do
+	docker cp $CONTAINER:$deb ./$ARTIFACT_DIR
+done
