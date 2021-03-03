cd55be5633f527f266de19fa6ca0da1de67cd4d9
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-03 11:22:54 -0600

    reordered commands in extract_artifacts.sh to create container before running commands in it

--- docker/extract_artifacts.sh
+++ docker/extract_artifacts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -eo pipefail
+set -euxo pipefail
 
 IMAGE=${1:-platform-sdk.ubuntu}
 ARTIFACT_DIR="artifact_dir"
 
 mkdir -p ./$ARTIFACT_DIR
 
-DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find . -name airmap-platform-sdk*.deb")
 CONTAINER=$(docker create $IMAGE)
+DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find . -name airmap-platform-sdk*.deb")
 for deb in $DEBS; do
 	docker cp $CONTAINER:$deb ./$ARTIFACT_DIR
 done
