c43d15b596fbf1a8a672d2d3d73bc9068a8bc800
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-03 17:28:42 -0600

    get artifacts into the right place to publish

--- docker/extract_artifacts.sh
+++ docker/extract_artifacts.sh
@@ -3,12 +3,12 @@
 set -euxo pipefail
 
 IMAGE=${1:-platform-sdk.ubuntu}
-ARTIFACT_DIR="artifact_dir"
+ARTIFACT_DIR="/usr/src/app/artifacts"
 
-mkdir -p ./$ARTIFACT_DIR
+mkdir -p $ARTIFACT_DIR
 
 CONTAINER=$(docker create $IMAGE)
-DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find . -name airmap-platform-sdk*.deb")
+DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find /usr/src/app/build -name airmap-platform-sdk*.deb")
 for deb in $DEBS; do
-	docker cp $CONTAINER:$deb ./$ARTIFACT_DIR
+	docker cp $CONTAINER:$deb $ARTIFACT_DIR
 done
