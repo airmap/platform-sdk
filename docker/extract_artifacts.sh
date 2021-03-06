fabb3acc2820943fbfcc9b94e776d34b154f3b1d
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-06 11:10:59 -0600

    pass azure artifact dir to script to extract artifacts from docker

--- docker/extract_artifacts.sh
+++ docker/extract_artifacts.sh
@@ -3,12 +3,12 @@
 set -euxo pipefail
 
 IMAGE=${1:-platform-sdk.ubuntu}
-ARTIFACT_DIR="/usr/src/app/artifacts"
+ARTIFACT_DIR=$2
 
 mkdir -p $ARTIFACT_DIR
 
 CONTAINER=$(docker create $IMAGE)
-DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find /usr/src/app/build -name airmap-platform-sdk*.deb")
+DEBS=$(docker run -i -a stdout $IMAGE /bin/bash -c "find /usr/src/app -name airmap-platform-sdk*.deb")
 for deb in $DEBS; do
 	docker cp $CONTAINER:$deb $ARTIFACT_DIR
 done
