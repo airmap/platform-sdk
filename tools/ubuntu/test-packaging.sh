ad329a40af04c7bdbeb6433ea78ed801a202fa2f
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-11 10:19:46 -0600

    tests for deb file

--- tools/ubuntu/test-packaging.sh
+++ tools/ubuntu/test-packaging.sh
@@ -3,5 +3,5 @@
 set -ex
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-#-- assumption that `make package` already ran and deposited the artifacts in the build directory
-#RUN find . -maxdepth 1 -name "airmap-platform-sdk*.deb" -exec dpkg -c {} +
+#-- assumption that `make package` already ran and deposited the artifacts in the image's /tmp directory
+find /tmp -maxdepth 1 -iname "airmap-platform-sdk*.deb" -exec dpkg -c {} +
