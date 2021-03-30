9a498ccb431ea46bf372d2b017940cb06d741b15
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-30 16:13:38 -0500

    disabled QT by default; added L4T build to azure pipeline; add L4T packaging as a .deb

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -exo
+set -eux
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $HERE
 
 build_opts="--force-rm=true $@"
-sudo docker run --rm --privileged multiarch/qemu-user-static:register || true
-sudo docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t . 
-sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker run --rm --privileged multiarch/qemu-user-static:register || true
+docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t . 
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
