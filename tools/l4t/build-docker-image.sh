3304fe5e65754c6996c36bcaf34963013f211e77
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-31 11:27:01 -0500

    reworking qemu registration to get it working in azure pipeline

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -4,6 +4,6 @@ set -eux
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 build_opts="--force-rm=true $@"
-docker run --rm --privileged multiarch/qemu-user-static:register || true
+#docker run --rm --privileged multiarch/qemu-user-static:register || true
+docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
 docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t . 
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
