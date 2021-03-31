cae3d1d4276b7356f84cfae869cd4ff1abd57f0f
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-31 11:51:45 -0500

    qemu registration better pipeline diagnostics

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -6,9 +6,9 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 build_opts="--force-rm=true $@"
 #docker run --rm --privileged multiarch/qemu-user-static:register || true
 docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
-systemctl status systemd-binfmt
 git clone https://github.com/computermouth/qemu-static-conf.git
 sudo mkdir -p /lib/binfmt.d
 sudo cp qemu-static-conf/*.conf /lib/binfmt.d/
 sudo systemctl restart systemd-binfmt.service
+systemctl status systemd-binfmt
 docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t . 
