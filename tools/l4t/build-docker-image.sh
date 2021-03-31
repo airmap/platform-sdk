ca0faa6afd3d316a4cae8240c3828d96801bfd96
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-31 14:31:27 -0500

    explicitly set platform for docker

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -5,6 +5,6 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 build_opts="--force-rm=true $@"
 docker version
-docker run --rm --privileged multiarch/qemu-user-static:register || true
+docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
 docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
-docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
+docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
