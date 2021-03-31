40a2bf35cb83b83d1a8d21d41b38c11f09db3c74
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-31 15:27:40 -0500

    well

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -4,7 +4,7 @@ set -eux
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 build_opts="--force-rm=true $@"
-docker version
 docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
-docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
+systemctl status systemd-binfmt
+docker build ${build_opts} --platform=linux/arm64 -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
 docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
