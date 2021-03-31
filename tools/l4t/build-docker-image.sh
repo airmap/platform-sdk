10a26be19323f5340edccacfd51590579d76629f
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-03-31 11:44:01 -0500

    qemu registration supplement

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -4,7 +4,11 @@ set -eux
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 build_opts="--force-rm=true $@"
-systemctl status systemd-binfmt
 #docker run --rm --privileged multiarch/qemu-user-static:register || true
 docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes
+systemctl status systemd-binfmt
+git clone https://github.com/computermouth/qemu-static-conf.git
+sudo mkdir -p /lib/binfmt.d
+sudo cp qemu-static-conf/*.conf /lib/binfmt.d/
+sudo systemctl restart systemd-binfmt.service
 docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t . 
