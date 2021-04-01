800d8aa6651e21a0ada0565339a472fdce567257
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-04-01 16:45:18 -0500

    supplemental config files

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -9,10 +9,10 @@ sudo apt-get install qemu binfmt-support qemu-user-static
 build_opts="--force-rm=true $@"
 #docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
 docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
-#git clone https://github.com/computermouth/qemu-static-conf.git
-#sudo mkdir -p /usr/lib/binfmt.d
-#sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
-#sudo systemctl restart systemd-binfmt.service || true
+git clone https://github.com/computermouth/qemu-static-conf.git
+sudo mkdir -p /usr/lib/binfmt.d
+sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
+sudo systemctl restart systemd-binfmt.service || true
 sudo systemctl status systemd-binfmt || true
-Sjournalctl -b -g binfmt || true
+#journalctl -b -g binfmt || true
 sudo docker build ${build_opts} --platform=linux/arm64 -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
