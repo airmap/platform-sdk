6e5fb1c4316089ba4149687df2917a4cdee7b9ae
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-04-01 16:39:50 -0500

    apt install more qemu

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -4,15 +4,15 @@ set -eux
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 uname -r
-sudo apt-get install qemu
+sudo apt-get install qemu binfmt-support qemu-user-static
 
 build_opts="--force-rm=true $@"
-sudo docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
+#docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
+docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
 #git clone https://github.com/computermouth/qemu-static-conf.git
 #sudo mkdir -p /usr/lib/binfmt.d
 #sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
 #sudo systemctl restart systemd-binfmt.service || true
 sudo systemctl status systemd-binfmt || true
-journalctl -b -g binfmt || true
+Sjournalctl -b -g binfmt || true
 sudo docker build ${build_opts} --platform=linux/arm64 -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
-sudo docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
