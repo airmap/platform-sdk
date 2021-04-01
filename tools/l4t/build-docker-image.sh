be39ac733223a920d7d3b360b6b65794bccd3c4f
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-04-01 15:01:35 -0500

    change config file installation dir

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -3,13 +3,15 @@
 set -eux
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+uname -r
+
 build_opts="--force-rm=true $@"
 git clone https://github.com/computermouth/qemu-static-conf.git
-sudo mkdir -p /lib/binfmt.d
-sudo cp qemu-static-conf/*.conf /lib/binfmt.d/
+sudo mkdir -p /usr/lib/binfmt.d
+sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
 sudo systemctl restart systemd-binfmt.service || true
 systemctl status systemd-binfmt || true
-journalctl -xe || true
+journalctl -b -g binfmt || true
 docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
 docker build ${build_opts} --platform=linux/arm64 -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
 docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
