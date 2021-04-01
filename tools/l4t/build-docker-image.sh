e420433eeb50fae34b89dd8f48c70d6742d22c01
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-04-01 15:27:36 -0500

    OYE!

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -7,11 +7,11 @@ uname -r
 
 build_opts="--force-rm=true $@"
 sudo docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
-git clone https://github.com/computermouth/qemu-static-conf.git
-sudo mkdir -p /usr/lib/binfmt.d
-sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
-sudo systemctl restart systemd-binfmt.service || true
-sudo systemctl status systemd-binfmt || true
-journalctl -b -g binfmt || true
+#git clone https://github.com/computermouth/qemu-static-conf.git
+#sudo mkdir -p /usr/lib/binfmt.d
+#sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
+#sudo systemctl restart systemd-binfmt.service || true
+#sudo systemctl status systemd-binfmt || true
+#journalctl -b -g binfmt || true
 sudo docker build ${build_opts} --platform=linux/arm64 -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
 sudo docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register --reset -p yes
