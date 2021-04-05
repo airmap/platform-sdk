10063261dca40f82eff91c665fe81abe9e41966d
Jenner Lochridge (jennerl github gpg keys) <56317773+jennerl@users.noreply.github.com>
2021-04-05 15:39:41 -0500

    L4T only builds on merge to master now; removed extraneous commands from L4T docker build

--- tools/l4t/build-docker-image.sh
+++ tools/l4t/build-docker-image.sh
@@ -7,12 +7,5 @@ uname -r
 sudo apt-get install qemu binfmt-support qemu-user-static
 
 build_opts="--force-rm=true $@"
-#docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static:register || true
 docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static --reset -p yes
-git clone https://github.com/computermouth/qemu-static-conf.git
-sudo mkdir -p /usr/lib/binfmt.d
-sudo cp qemu-static-conf/*.conf /usr/lib/binfmt.d/
-sudo systemctl restart systemd-binfmt.service || true
-sudo systemctl status systemd-binfmt || true
-#journalctl -b -g binfmt || true
-sudo docker build ${build_opts} --platform=linux/arm64 -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
+sudo docker build ${build_opts} -t airmapd-l4t:latest -f $HERE/../../docker/l4t .
